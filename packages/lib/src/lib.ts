8d673b18de74a039025781a117b875787f1b53fd
Ian Mancini <ian.mancini.perez@gmail.com>
2023-05-15 15:36:17 +0200

    feat(lib): add function to get save type

--- packages/lib/src/lib.ts
+++ packages/lib/src/lib.ts
@@ -1,4 +1,14 @@
-enum SaveType {
+export enum SaveType {
   WiiU = 0x1b470000,
   Switch = 0x0000471b,
 }
+
+export const getSaveType = (buffer: ArrayBuffer): SaveType => {
+  const type = new Uint32Array(buffer, 0, 4);
+
+  if (!Object.values(SaveType).includes(type[0])) {
+    throw new TypeError("The provided input is not a a valid option.sav file");
+  }
+
+  return type[0];
+};

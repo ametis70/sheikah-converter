8bc6a6dfc2b3e3af342aeef7b73736ab079a2abf
Ian Mancini <ian.mancini.perez@gmail.com>
2024-03-15 18:44:33 -0300

    feat(lib): add rollup for compiling esm and cjs bundles

---
+++ packages/lib/rollup.config.mjs
@@ -0,0 +1,21 @@
+import typescript from "@rollup/plugin-typescript";
+
+/**
+ * @type {import('rollup').RollupOptions}
+ */
+export default {
+  input: "src/index.ts",
+  output: [
+    {
+      file: "dist/index.js",
+      format: "cjs",
+      sourcemap: true,
+    },
+    {
+      file: "dist/index.esm.js",
+      format: "esm",
+      sourcemap: true,
+    },
+  ],
+  plugins: [typescript()],
+};
